22e23983d9bcb629601af09a792f2bb24ced9d59
Felipe Gasper <felipe@felipegasper.com>
2017-09-28 01:33:10 -0400

    kill it

--- build/compile.js
+++ build/compile.js
@@ -16,14 +16,22 @@ var SOURCES = [
     "zsession",
     "zerror",
     "zvalidation",
-    "zbrowser",
 ];
 
-SOURCES = SOURCES.map( (s) => { return `src/${s}.js` } );
+var BROWSER_SOURCES = SOURCES.slice(0).concat( ["zbrowser"] );
 
-concat( SOURCES, 'zmodem.js' );
+const nameroot_sources = new Map( [
+    [ "zmodem-pure", SOURCES ],
+    [ "zmodem", BROWSER_SOURCES ],
+] );
 
-concat( SOURCES ).then( (alljs) => {
-    const {code} = minify(alljs);
-    fs.writeFileSync( 'zmodem-min.js', code );
-} ).catch( console.error.bind(console) );
+for ( let [base, sources] of nameroot_sources ) {
+    var full_sources = sources.map( (s) => { return `src/${s}.js` } );
+
+    concat( full_sources, `${base}.js` );
+
+    concat( full_sources ).then( (alljs) => {
+        const {code} = minify(alljs);
+        fs.writeFileSync( `${base}-min.js`, code );
+    } ).catch( console.error.bind(console) );
+}
